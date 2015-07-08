1199d572bbb1772951cd2b1d02f448b6be4e050b
ohtomi <ohtomi.kenichi@gmail.com>
2015-07-09 00:10:04 +0900

    feat: sandbox for electron

---
+++ sandbox-electron/src/main/js/main.js
@@ -0,0 +1,22 @@
+// main.js
+var app = require('app');
+var BrowserWindow = require('browser-window');
+
+require('crash-reporter').start();
+
+var mainWindow = null;
+
+app.on('window-all-closed', function() {
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('ready', function() {
+  mainWindow = new BrowserWindow({width: 800, heigth: 600});
+  mainWindow.loadUrl('file://' + __dirname + '/../html/index.html');
+  mainWindow.openDevTools();
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
+});
