1e6633a187471991c4808b13ad54f31f9c7eb63a
ohtomi <ohtomi.kenichi@gmail.com>
2015-07-18 14:02:55 +0900

    refactor: extract local variable

--- sandbox-spring-reactor/src/main/java/com/example/Main.java
+++ sandbox-spring-reactor/src/main/java/com/example/Main.java
@@ -1,24 +1,34 @@
 package com.example;
 
+import java.util.stream.Stream;
+
 import reactor.Environment;
 import reactor.rx.broadcast.Broadcaster;
 
 public class Main {
+
+	private static final String[] NAMES;
+	static {
+		NAMES = new String[] { "Alice", "Bob", "Charles" };
+	}
 
 	private static void helloWorld() {
 		System.out.println("\n--- helloWorld ---\n");
 
 		Environment.initialize();
 		Broadcaster<String> sink = Broadcaster.create(Environment.get());
-		sink.dispatchOn(Environment.cachedDispatcher()) //
-				.map(String::toUpperCase) //
-				.consume(System.out::println);
-		sink.onNext("Hello World!");
+		sink.dispatchOn(Environment.cachedDispatcher())
+				.map(String::toUpperCase).consume(name -> {
+					System.out.println("Hello " + name + "!");
+				});
+
+		Stream.of(NAMES).forEach(name -> sink.onNext(name));
+		sink.onComplete();
+		sink.join();
 	}
 
 	public static void main(String... args) throws InterruptedException {
 		helloWorld();
-		Thread.sleep(500);
 	}
 
 }
