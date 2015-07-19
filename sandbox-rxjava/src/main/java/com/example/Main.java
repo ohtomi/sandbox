a5c972e7d56d5cb7c3b61fd4e5eaa795337fc290
ohtomi <ohtomi.kenichi@gmail.com>
2015-07-19 22:13:04 +0900

    refactor: remove throw statement

--- sandbox-rxjava/src/main/java/com/example/Main.java
+++ sandbox-rxjava/src/main/java/com/example/Main.java
@@ -1,43 +1,24 @@
 package com.example;
 
-import static java.util.Comparator.nullsLast;
-import static java.util.Comparator.reverseOrder;
-
-import java.util.stream.Stream;
-
 import rx.Observable;
 
 public class Main {
 
-	private static void helloWorld(String... names) {
-		System.out.println("\n--- helloWorld ---\n");
+    private static final String[] NAMES;
+    static {
+        NAMES = new String[] { "Alice", "Bob", "Charles" };
+    }
 
-		Observable.from(names).subscribe(name -> {
-			System.out.println("Hello " + name + "!");
-		});
-	}
+    private static void helloWorld() {
+        System.out.println("\n--- helloWorld ---\n");
 
-	private static void syncObservable() {
-		System.out.println("\n--- syncObservable ---\n");
+        Observable.from(NAMES).map(String::toUpperCase).subscribe(name -> {
+            System.out.println("Hello " + name + "!");
+        });
+    }
 
-		Observable<String> observable = Observable.create(subscriber -> {
-			Stream.of("Alice", "Bob", null, "Charles")
-					.sorted(nullsLast(reverseOrder())).forEach(name -> {
-						if (!subscriber.isUnsubscribed()) {
-							subscriber.onNext("Hello " + name + "!");
-						}
-					});
-			if (!subscriber.isUnsubscribed()) {
-				subscriber.onCompleted();
-			}
-		});
-
-		observable.subscribe(System.out::println);
-	}
-
-	public static void main(String... args) {
-		helloWorld(new String[] { "Alice", "Bob", "Charles", });
-		syncObservable();
-	}
+    public static void main(String... args) {
+        helloWorld();
+    }
 
 }
--- sandbox-spring-reactor/src/main/java/com/example/Main.java
+++ sandbox-spring-reactor/src/main/java/com/example/Main.java
@@ -7,28 +7,27 @@ import reactor.rx.broadcast.Broadcaster;
 
 public class Main {
 
-	private static final String[] NAMES;
-	static {
-		NAMES = new String[] { "Alice", "Bob", "Charles" };
-	}
+    private static final String[] NAMES;
+    static {
+        NAMES = new String[] { "Alice", "Bob", "Charles" };
+    }
 
-	private static void helloWorld() {
-		System.out.println("\n--- helloWorld ---\n");
+    private static void helloWorld() {
+        System.out.println("\n--- helloWorld ---\n");
 
-		Environment.initialize();
-		Broadcaster<String> sink = Broadcaster.create(Environment.get());
-		sink.dispatchOn(Environment.cachedDispatcher())
-				.map(String::toUpperCase).consume(name -> {
-					System.out.println("Hello " + name + "!");
-				});
+        Environment.initialize();
+        Broadcaster<String> sink = Broadcaster.create(Environment.get());
+        sink.dispatchOn(Environment.cachedDispatcher()).map(String::toUpperCase).consume(name -> {
+            System.out.println("Hello " + name + "!");
+        });
 
-		Stream.of(NAMES).forEach(name -> sink.onNext(name));
-		sink.onComplete();
-		sink.join();
-	}
+        Stream.of(NAMES).forEach(name -> sink.onNext(name));
+        sink.onComplete();
+        sink.join();
+    }
 
-	public static void main(String... args) throws InterruptedException {
-		helloWorld();
-	}
+    public static void main(String... args) {
+        helloWorld();
+    }
 
 }
