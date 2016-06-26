b262371cddb73512b86a0d5461e561e3b504962a
ohtomi <ohtomi.kenichi@gmail.com>
2016-06-27 06:47:32 +0900

    Write sample codes

--- sandbox-kotlin/src/Classes.kt
+++ sandbox-kotlin/src/Classes.kt
@@ -56,6 +56,16 @@ private fun classes_class_2(): Unit {
     println("name: ${person.lastName} ${person.firstName}, age: ${person.age}")
 }
 
+private interface B {
+    fun f() {
+        println("B")
+    }
+
+    fun b() {
+        println("b")
+    }
+}
+
 private fun classes_inherit(): Unit {
     open class Base(p: Int)
     class Derived(p: Int) : Base(p)
@@ -77,6 +87,152 @@ private fun classes_inherit(): Unit {
     val v = MyView(Context(), AttributeSet())
     println(v)
 
+    open class Base2 {
+        open fun v() {
+        }
+
+        fun nv() {
+        }
+    }
+
+    class Derived2() : Base2() {
+        override fun v() {
+        }
+    }
+
+    open class AnotherDerived() : Base2() {
+        final override fun v() {
+        }
+    }
+
+    open class A {
+        open fun f() {
+            println("A")
+        }
+
+        fun a() {
+            println("a")
+        }
+    }
+
+    class C() : A(), B {
+        override fun f() {
+            super<A>.f()
+            super<B>.f()
+        }
+    }
+
+    C().f()
+}
+
+fun classes_abstract(): Unit {
+    open class Base {
+        open fun f() {
+        }
+    }
+
+    abstract class Derived : Base() {
+        override abstract fun f()
+    }
+
+}
+
+sealed class Expr {
+    class Const(val number: Double) : Expr()
+    class Sum(val e1: Expr, val e2: Expr) : Expr()
+
+    object NotANumber : Expr()
+}
+
+fun classes_sealed(): Unit {
+    fun eval(expr: Expr): Double = when (expr ) {
+        is Expr.Const -> expr.number
+        is Expr.Sum -> eval(expr.e1) + eval(expr.e2)
+        Expr.NotANumber -> Double.NaN
+    }
+
+    println(eval(Expr.Sum(Expr.Const(2.0), Expr.Const(3.0))))
+}
+
+private const val SUBSYSTEM_DEPRECATED: String = "This subsystem is deperecated"
+
+private object O {
+    const val SUBSYSTEM_DEPRECATED: String = "This subsystem is deperecated"
+}
+
+fun classes_property(): Unit {
+    class A {
+        //var allByDefault: Int?
+        var initialized = 1
+        var stringRepresentation: String
+            get() = this.toString()
+            set(value) {
+                field = "+" ; value + "+"
+            }
+        var setterVisiblity: String = "abc"
+            private set
+        var setterWithAnnotation: Any?
+            @Inject set
+
+        val simple: Int?
+        val inferredType = 1
+        val isEmpty: Boolean
+            get() = inferredType != 1
+
+        val table: Map<String, Int>? = null
+            get() {
+                if (field == null)
+                    field = HashMap()
+                return field
+            }
+
+        init {
+            stringRepresentation = ""
+            simple = 0
+            setterWithAnnotation = null
+        }
+    }
+
+    val table = A().table
+    println(table)
+
+    class TestSubject {
+        fun method() {
+        }
+    }
+
+    class MyTest {
+        lateinit var subject: TestSubject
+        fun setup() {
+            subject = TestSubject()
+        }
+
+        fun test() {
+            subject.method()
+        }
+    }
+
+    open class Person(open val name: String) {
+    }
+
+    class UpperCaseNamePerson(name: String) : Person(name) {
+        override val name: String
+            get() {
+                return super.name.toUpperCase()
+            }
+    }
+
+    println(Person("kenichi ohtomi").name)
+    println(UpperCaseNamePerson("kenichi ohtomi").name)
+
+    class User(val map: Map<String, Any?>) {
+        val name: String by map
+        val age: Int by map
+    }
+
+    val user = User(mapOf("name" to "John Doe", "age" to 25))
+    println(user.name)
+    println(user.age)
 }
 
 
@@ -86,4 +242,7 @@ fun classes_run(): Unit {
     classes_class_1()
     classes_class_2()
     classes_inherit()
+    classes_abstract()
+    classes_sealed()
+    classes_property()
 }
