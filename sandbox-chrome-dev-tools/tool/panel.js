09f5831519bb90467dca3d7cccee34b22e3defb6
ohtomi <ohtomi.kenichi@gmail.com>
2017-09-24 22:11:30 +0900

    Update sample of google chrome developer tools

--- sandbox-chrome-dev-tools/tool/panel.js
+++ sandbox-chrome-dev-tools/tool/panel.js
@@ -2,14 +2,6 @@ console.warn('panel.js');
 
 const messagesEl = document.getElementById('messages');
 
-// chrome.devtools.network.onRequestFinished.addListener((req) => {
-//     const url = req.request.url;
-//     if (url.indexOf('twitter') !== -1) {
-//         console.log(`req:${JSON.stringify(req)}`);
-//         document.getElementById('message').innerText = 'このサイトから、Twitterの香りがするでゲソ';
-//     }
-// });
-
 chrome.devtools.network.onNavigated.addListener((url) => {
     console.clear();
     messagesEl.textContent = '';
