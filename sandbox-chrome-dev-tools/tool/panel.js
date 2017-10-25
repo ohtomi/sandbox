962312b64294619a69e54ec7ca2765e574fa841c
ohtomi <ohtomi.kenichi@gmail.com>
2017-10-25 21:12:07 +0900

    Show click events on main panel

--- sandbox-chrome-dev-tools/tool/panel.js
+++ sandbox-chrome-dev-tools/tool/panel.js
@@ -27639,65 +27639,9 @@ var styles = {
     }),
     container: RX.Styles.createViewStyle({
         padding: 16,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }),
-    container2: RX.Styles.createViewStyle({
-        padding: 16,
         justifyContent: 'flex-start',
         alignItems: 'flex-start'
-    }),
-    helloWorld: RX.Styles.createTextStyle({
-        fontSize: 48,
-        fontWeight: 'bold',
-        marginBottom: 28
-    }),
-    welcome: RX.Styles.createTextStyle({
-        fontSize: 32,
-        marginBottom: 12
-    }),
-    instructions: RX.Styles.createTextStyle({
-        fontSize: 16,
-        color: '#aaa',
-        marginBottom: 16
-    }),
-    docLink: RX.Styles.createLinkStyle({
-        fontSize: 16,
-        color: 'blue',
-        marginBottom: 16
-    }),
-    inputBox: RX.Styles.createTextInputStyle({
-        width: 400,
-        marginTop: 8,
-        marginBottom: 8,
-        padding: 4
-    }),
-    picker: RX.Styles.createPickerStyle({
-        width: 400,
-        marginTop: 8,
-        marginBottom: 8
-    }),
-    roundButton: RX.Styles.createViewStyle({
-        margin: 16,
-        borderRadius: 16,
-        backgroundColor: '#7d88a9'
-    }),
-    buttonText: RX.Styles.createTextStyle({
-        fontSize: 16,
-        marginVertical: 6,
-        marginHorizontal: 12,
-        color: 'white'
     })
-};
-var marks = {
-    closed: {
-        head: '\u25b8',
-        body: 'ほげ ふが ...'
-    },
-    opened: {
-        head: '\u25be',
-        body: 'ほげ ふが！！！\nふー ばー ばず！！！'
-    }
 };
 var MainPanel = (function (_super) {
     __extends(MainPanel, _super);
@@ -27706,84 +27650,44 @@ var MainPanel = (function (_super) {
         _this._onMessageFromDevTools = function (message) {
             var newState = _this.state;
             newState.listItems.push({
-                label: new Date().toUTCString(),
-                value: JSON.stringify(message)
+                open: false,
+                date: new Date().toUTCString(),
+                text: JSON.stringify(message)
             });
             _this.setState(newState);
         };
-        _this._onPressClickableText = function (e) {
+        _this._onPressClickableText = function (index, e) {
             var newState = _this.state;
-            newState.markState = !newState.markState;
+            newState.listItems[index].open = !newState.listItems[index].open;
             _this.setState(newState);
-        };
-        _this._onChangeText = function (newValue) {
-            var newState = _this.state;
-            newState.inputValue = newValue;
-            _this.setState(newState);
-        };
-        _this._onValueChange = function (itemValue, itemPosition) {
-            var newState = _this.state;
-            newState.selectedValue = itemValue;
-            _this.setState(newState);
-        };
-        _this._onPressNavigate = function () {
-            _this.props.onPressNavigate();
-        };
-        _this._onPressAlert = function () {
-            RX.Alert.show('警告', new Date() + " \u30C0\u30A4\u30A2\u30ED\u30B0\u30E1\u30C3\u30BB\u30FC\u30B8 \u30C0\u30A4\u30A2\u30ED\u30B0\u30E1\u30C3\u30BB\u30FC\u30B8");
         };
         _this.state = {
-            markState: true,
-            inputValue: '',
-            selectedValue: 'value-2',
-            listItems: [
-                { label: 'label-1', value: 'value-1' },
-                { label: 'label-2', value: 'value-2' },
-                { label: 'label-3', value: 'value-3' },
-                { label: 'label-4', value: 'value-4' },
-                { label: 'label-5', value: 'value-5' }
-            ]
+            listItems: []
         };
-        _this._translationValue = new RX.Animated.Value(-100);
-        _this._animatedStyle = RX.Styles.createAnimatedTextStyle({
-            transform: [
-                {
-                    translateY: _this._translationValue
-                }
-            ]
-        });
         return _this;
     }
     MainPanel.prototype.componentDidMount = function () {
         this.props.emitter.on('from-devtools', this._onMessageFromDevTools);
-        var animation = RX.Animated.timing(this._translationValue, {
-            toValue: 0,
-            easing: RX.Animated.Easing.OutBack(),
-            duration: 500
-        });
-        animation.start();
     };
     MainPanel.prototype.render = function () {
+        var _this = this;
+        var items = this.state.listItems.map(function (listItem, index) {
+            if (listItem.open) {
+                return (RX.createElement(RX.Text, { key: index, onPress: _this._onPressClickableText.bind(_this, index) },
+                    '\u25be ',
+                    RX.createElement(RX.Text, null, listItem.date),
+                    RX.createElement("br", null),
+                    '  ',
+                    RX.createElement(RX.Text, null, listItem.text)));
+            }
+            else {
+                return (RX.createElement(RX.Text, { key: index, onPress: _this._onPressClickableText.bind(_this, index) },
+                    '\u25b8 ',
+                    RX.createElement(RX.Text, null, listItem.date)));
+            }
+        });
         return (RX.createElement(RX.ScrollView, { style: styles.scroll },
-            RX.createElement(RX.View, { style: styles.container },
-                RX.createElement(RX.Animated.Text, { style: [styles.helloWorld, this._animatedStyle] }, "Hello World"),
-                RX.createElement(RX.Text, { style: styles.welcome }, "Welcome to ReactXP"),
-                RX.createElement(RX.Text, { style: styles.instructions }, "Edit App.tsx to get started"),
-                RX.createElement(RX.Link, { style: styles.docLink, url: 'https://microsoft.github.io/reactxp/docs' }, "View ReactXP documentation"),
-                RX.createElement(RX.TextInput, { style: styles.inputBox, autoFocus: true, placeholder: '何かを入力すべし', value: this.state.inputValue, onChangeText: this._onChangeText }),
-                RX.createElement(RX.Picker, { style: styles.picker, items: this.state.listItems, selectedValue: this.state.selectedValue, onValueChange: this._onValueChange }),
-                RX.createElement(RX.ActivityIndicator, { color: ('black'), size: 'tiny' }),
-                RX.createElement(RX.Button, { style: styles.roundButton, onPress: this._onPressNavigate },
-                    RX.createElement(RX.Text, { style: styles.buttonText }, "See More Examples")),
-                RX.createElement(RX.Button, { style: styles.roundButton, onPress: this._onPressAlert },
-                    RX.createElement(RX.Text, { style: styles.buttonText }, "Open Alert"))),
-            RX.createElement("hr", null),
-            RX.createElement(RX.View, { style: styles.container2 },
-                RX.createElement(RX.Text, { onPress: this._onPressClickableText },
-                    this.state.markState ? marks.closed.head : marks.opened.head,
-                    RX.createElement(RX.Text, null,
-                        " ",
-                        this.state.markState ? marks.closed.body : marks.opened.body)))));
+            RX.createElement(RX.View, { style: styles.container }, items)));
     };
     return MainPanel;
 }(RX.Component));
