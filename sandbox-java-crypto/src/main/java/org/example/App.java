5f90a430e04f948c9943b4cc5a6112da146040f8
ohtomi <ohtomi.kenichi@gmail.com>
2017-10-31 21:51:40 +0900

    Add micro benchmark of java crypto framework

--- sandbox-java-crypto/src/main/java/org/example/App.java
+++ sandbox-java-crypto/src/main/java/org/example/App.java
@@ -34,28 +34,51 @@ public class App {
     public static void main(String... args) throws Exception {
         App app = new App();
 
-        String original = "your name is";
-        byte[] input = original.getBytes(StandardCharsets.UTF_8);
-        byte[] encrpyted = app.encrypt(input);
-        byte[] decrpypted = app.decrypt(encrpyted);
-        String result = new String(decrpypted, StandardCharsets.UTF_8);
+        {
+            String original = "your name is";
+            byte[] input = original.getBytes(StandardCharsets.UTF_8);
+            byte[] encrpyted = app.encrypt(input);
+            byte[] decrpypted = app.decrypt(encrpyted);
+            String result = new String(decrpypted, StandardCharsets.UTF_8);
 
-        System.out.println(String.format(" original: '%s'", original));
-        System.out.println(String.format("encrypted: '%s'", Base64.getEncoder().encodeToString(encrpyted)));
-        System.out.println(String.format("decrypted: '%s'", Base64.getEncoder().encodeToString(decrpypted)));
-        System.out.println(String.format("   result: '%s'", result));
-        System.out.println(String.format("            123456789 123456789 123456789 123456789"));
+            System.out.println(String.format(" original: '%s'", original));
+            System.out.println(String.format("encrypted: '%s'", Base64.getEncoder().encodeToString(encrpyted)));
+            System.out.println(String.format("decrypted: '%s'", Base64.getEncoder().encodeToString(decrpypted)));
+            System.out.println(String.format("   result: '%s'", result));
+            System.out.println(String.format("            123456789 123456789 123456789 123456789"));
+        }
 
         int loop = 100000;
-        byte[] headOfReturns = new byte[loop];
-        long from = System.currentTimeMillis();
-        for (int i = 0; i < loop; i++) {
-            byte[] ret = app.encrypt(String.format("hoge-%d", i).getBytes(StandardCharsets.UTF_8));
-            headOfReturns[i] = ret[0];
-        }
-        long to = System.currentTimeMillis();
 
-        System.out.println();
-        System.out.println(String.format("%d times, elapsed: %d msec,\nreturns: %s", loop, to - from, Base64.getEncoder().encodeToString(headOfReturns)));
+        {
+            byte[] headOfReturns = new byte[loop];
+            long from = System.currentTimeMillis();
+            for (int i = 0; i < loop; i++) {
+                byte[] ret = app.encrypt(String.format("hoge-%d", i).getBytes(StandardCharsets.UTF_8));
+                headOfReturns[i] = ret[0];
+            }
+            long to = System.currentTimeMillis();
+
+            System.out.println();
+            System.out.println(String.format("%d times, elapsed: %d msec,\nreturns: %s", loop, to - from, Base64.getEncoder().encodeToString(headOfReturns)));
+        }
+        {
+            SecretKeySpec key = new SecretKeySpec(App.key, "AES");
+            IvParameterSpec iv = new IvParameterSpec(App.iv);
+
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, key, iv);
+
+            byte[] headOfReturns = new byte[loop];
+            long from = System.currentTimeMillis();
+            for (int i = 0; i < loop; i++) {
+                byte[] ret = cipher.doFinal(String.format("hoge-%d", i).getBytes(StandardCharsets.UTF_8));
+                headOfReturns[i] = ret[0];
+            }
+            long to = System.currentTimeMillis();
+
+            System.out.println();
+            System.out.println(String.format("%d times, elapsed: %d msec,\nreturns: %s", loop, to - from, Base64.getEncoder().encodeToString(headOfReturns)));
+        }
     }
 }
