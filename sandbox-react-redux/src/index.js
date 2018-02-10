b69940655a5d1767fd8784b12df5219ad2e0b3ae
ohtomi <ohtomi.kenichi@gmail.com>
2018-02-11 00:47:07 +0900

    Use redux-logger

--- sandbox-react-redux/src/index.js
+++ sandbox-react-redux/src/index.js
@@ -5,13 +5,14 @@ import AppContainer from './components/AppContainer';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
+import logger from 'redux-logger'
 import reducers from './reducers';
 
 import './index.css';
 
 import registerServiceWorker from './registerServiceWorker';
 
-let store = createStore(reducers, applyMiddleware(thunkMiddleware));
+let store = createStore(reducers, applyMiddleware(thunkMiddleware, logger));
 
 ReactDOM.render(
     <Provider store={store}>
