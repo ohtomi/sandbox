a5c75a840f8969e60d9db9acb8961619392275ff
ohtomi <ohtomi.kenichi@gmail.com>
2018-07-14 00:50:34 +0900

    Upgrade rxjava from 1.x to 2.x

--- sandbox-rxjava/src/main/java/com/example/Main.java
+++ sandbox-rxjava/src/main/java/com/example/Main.java
@@ -1,121 +1,156 @@
 package com.example;
 
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.TimeUnit;
-
-import rx.Observable;
-import rx.subjects.AsyncSubject;
-import rx.subjects.BehaviorSubject;
-import rx.subjects.PublishSubject;
-import rx.subjects.ReplaySubject;
+import io.reactivex.Observable;
+import io.reactivex.Scheduler;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.internal.schedulers.SingleScheduler;
+import io.reactivex.subjects.ReplaySubject;
+import io.reactivex.subjects.Subject;
 
 public class Main {
 
-    private static void fromList() {
-        Observable<String> observable = Observable.from(new String[] { "Alice", "Bob", "Charles" });
-        observable.subscribe(s -> System.out.println("fromList: " + s), err -> err.printStackTrace());
+    private static Observable<Integer> createCounter() {
+        return Observable.range(1, 10);
     }
 
-    private static void publishSubject() {
-        PublishSubject<String> subject = PublishSubject.create();
-        subject.subscribe(s -> System.out.println("publishSubject 1: " + s), err -> err.printStackTrace());
-
-        CompletableFuture.runAsync(() -> {
-            for (int i = 0; i < 10; i++) {
-                try {
-                    TimeUnit.MILLISECONDS.sleep(30);
-                } catch (Exception ignore) {
-                }
-                subject.onNext(String.format("message(%d) from future", i));
-            }
-            subject.onCompleted();
+    private void sample1() throws Exception {
+        // 時間tを流れるintegerのstream
+        Observable<Integer> counter = Main.createCounter();
+        // 観測する
+        Disposable d = counter.subscribe(i -> {
+            System.out.println("sample1() => i: " + i);
         });
 
-        try {
-            TimeUnit.MILLISECONDS.sleep(200);
-        } catch (Exception ignore) {
-        }
-        subject.subscribe(s -> System.out.println("publishSubject 2: " + s), err -> err.printStackTrace());
+        Thread.sleep(100);
+        d.dispose();
     }
 
-    private static void asyncSubject() {
-        AsyncSubject<String> subject = AsyncSubject.create();
-        subject.subscribe(s -> System.out.println("asyncSubject 1: " + s), err -> err.printStackTrace());
-
-        CompletableFuture.runAsync(() -> {
-            for (int i = 0; i < 10; i++) {
-                try {
-                    TimeUnit.MILLISECONDS.sleep(30);
-                } catch (Exception ignore) {
-                }
-                subject.onNext(String.format("message(%d) from future", i));
-            }
-            subject.onCompleted();
-        });
-
-        try {
-            TimeUnit.MILLISECONDS.sleep(500);
-        } catch (Exception ignore) {
-        }
-        subject.subscribe(s -> System.out.println("asyncSubject 2: " + s), err -> err.printStackTrace());
+    private void sample2() throws Exception {
+        // 時間tを流れるintegerのstream
+        Observable<Integer> counter = Main.createCounter();
+        // 関数を適用する
+        Observable<String> countLog = counter.map(i -> "[" + i + "]");
+        String log = countLog
+                .reduce((a, b) -> a + ", " + b)
+                .toSingle()
+                .blockingGet();
+        System.out.println("sample2() => " + log);
     }
 
-    private static void replaySubject() {
-        ReplaySubject<String> subject = ReplaySubject.create();
-        subject.subscribe(s -> System.out.println("replaySubject 1: " + s), err -> err.printStackTrace());
-
-        CompletableFuture.runAsync(() -> {
-            for (int i = 0; i < 10; i++) {
-                try {
-                    TimeUnit.MILLISECONDS.sleep(30);
-                } catch (Exception ignore) {
-                }
-                subject.onNext(String.format("message(%d) from future", i));
-            }
-            subject.onCompleted();
-        });
-
-        try {
-            TimeUnit.MILLISECONDS.sleep(200);
-        } catch (Exception ignore) {
-        }
-        subject.subscribe(s -> System.out.println("replaySubject 2: " + s), err -> err.printStackTrace());
+    private static Observable<Integer> fromList(Integer... array) {
+        return Observable.fromArray(array);
     }
 
-    private static void behaviorSubject() {
-        BehaviorSubject<String> subject = BehaviorSubject.create();
-        Observable.combineLatest(subject.take(1), subject.skip(1), (a, b) -> {
-            return String.format("%s -> %s", a, b);
-        }).subscribe(s -> System.out.println("behaviorSubject: " + s));
+    private void sample3() throws Exception {
+        // Schedulerをセットする
+        Scheduler scheduler1 = new SingleScheduler();
+        Scheduler scheduler2 = new SingleScheduler();
+        Observable<Integer> counter = Main.fromList(1, 3, 5, 7, 9);
+        Disposable d = counter.subscribeOn(scheduler1)
+                .observeOn(scheduler2)
+                .subscribe(i -> {
+                    System.out.println("sample3() => i: " + i);
+                    scheduler1.shutdown();
+                    scheduler2.shutdown();
+                });
 
-        CompletableFuture.runAsync(() -> {
-            for (int i = 0; i < 10; i++) {
-                try {
-                    TimeUnit.MILLISECONDS.sleep(30);
-                } catch (Exception ignore) {
-                }
-                subject.onNext(String.format("message(%d) from future", i));
-            }
-            subject.onCompleted();
+        scheduler1.start();
+        scheduler2.start();
+
+        Thread.sleep(100);
+        d.dispose();
+    }
+
+    private void sample4() throws Exception {
+        // エラーハンドリングする
+        Observable<Integer> counter = Main.fromList(1, 3, 5, 7, 9);
+        Disposable d = counter.subscribe(i -> {
+            System.out.println("sample4() i: " + i);
+        }, th -> {
+            System.out.println("sample4() th: " + th.getMessage());
+            th.printStackTrace();
+        }, () -> {
+            System.out.println("sample4() done");
         });
+
+        Thread.sleep(100);
+        d.dispose();
+    }
+
+    private void sample5() throws Exception {
+        // Promise化でさよならAsyncTask
+        Scheduler scheduler = new SingleScheduler();
+        Observable<Integer> counter = Observable.just(13579);
+        Disposable d = counter.subscribeOn(scheduler)
+                .subscribe(i -> {
+                    System.out.println("sample5() i: " + i);
+                }, th -> {
+                    System.out.println("sample5() th: " + th.getMessage());
+                    th.printStackTrace();
+                }, () -> {
+                    scheduler.shutdown();
+                    System.out.println("sample5() done");
+                });
+
+        scheduler.start();
+
+        Thread.sleep(100);
+        d.dispose();
+    }
+
+    private void sample6() throws Exception {
+        // mergeを使った並列リクエスト
+        Observable<Object> counter = Observable.merge(
+                Observable.just(13579),
+                Observable.just("97531"));
+        Disposable d = counter.subscribe(o -> {
+            System.out.println("sample6() o: " + o.getClass().getCanonicalName());
+        }, th -> {
+            System.out.println("sample6() th: " + th.getMessage());
+            th.printStackTrace();
+        }, () -> {
+            System.out.println("sample6() done");
+        });
+
+        Thread.sleep(100);
+        d.dispose();
+    }
+
+    private void sample7() throws Exception {
+        // Subject
+        Observable<Integer> counter = Main.fromList(1, 3, 5, 7, 9);
+        Subject<Integer> replay = ReplaySubject.createWithSize(3);
+        Disposable d1 = counter.subscribe(i -> {
+            replay.onNext(i * i);
+        }, th -> {
+            replay.onError(th);
+            th.printStackTrace();
+        }, () -> {
+            System.out.println("sample7() eos");
+            replay.onComplete();
+        });
+        Disposable d2 = replay.subscribe(i -> {
+            System.out.println("sample7() i: " + i);
+        }, th -> {
+            System.out.println("sample7() th: " + th.getMessage());
+            th.printStackTrace();
+        }, () -> {
+            System.out.println("sample7() done");
+        });
+
+        Thread.sleep(100);
+        d1.dispose();
+        d2.dispose();
     }
 
     public static void main(String... args) throws Exception {
-        fromList();
-        join();
-        publishSubject();
-        join();
-        asyncSubject();
-        join();
-        replaySubject();
-        join();
-        behaviorSubject();
-        join();
+        Main app = new Main();
+        app.sample1();
+        app.sample2();
+        app.sample3();
+        app.sample4();
+        app.sample5();
+        app.sample6();
+        app.sample7();
     }
-
-    private static void join() throws Exception {
-        TimeUnit.SECONDS.sleep(1);
-        System.out.println();
-    }
-
 }
