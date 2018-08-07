2d1f0418a96349d1438dae39bbfbb25a61863f86
ohtomi <ohtomi.kenichi@gmail.com>
2018-08-07 21:57:45 +0900

    Register extension point of editor tab color

---
+++ sandbox-intellij-plugin/src/main/java/com/github/ohtomi/sandbox/MyEditorTabColorProvider.java
@@ -0,0 +1,29 @@
+package com.github.ohtomi.sandbox;
+
+import com.intellij.openapi.fileEditor.impl.EditorTabColorProvider;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.ui.JBColor;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.awt.*;
+
+public class MyEditorTabColorProvider implements EditorTabColorProvider {
+
+    @Nullable
+    @Override
+    public Color getEditorTabColor(@NotNull Project project, @NotNull VirtualFile file) {
+        System.out.println("getEditorTabColor(@NotNull Project project, @NotNull VirtualFile file)");
+        switch (file.getFileType().getDefaultExtension()) {
+            case "txt":
+                return JBColor.GREEN;
+            case "xml":
+                return JBColor.BLUE;
+            case "java":
+                return JBColor.YELLOW;
+            default:
+                return null;
+        }
+    }
+}
