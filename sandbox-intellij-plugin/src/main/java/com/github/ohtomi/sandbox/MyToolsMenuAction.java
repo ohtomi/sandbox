8e5b314f5dee6daa2712ca60a0ba16ebf87c4b29
ohtomi <ohtomi.kenichi@gmail.com>
2018-08-09 20:13:28 +0900

    Add custom action that adds menu item into Tools menu

---
+++ sandbox-intellij-plugin/src/main/java/com/github/ohtomi/sandbox/MyToolsMenuAction.java
@@ -0,0 +1,20 @@
+package com.github.ohtomi.sandbox;
+
+import com.intellij.icons.AllIcons;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.ui.Messages;
+
+public class MyToolsMenuAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        Messages.showInfoMessage("Hello World!", "Hello");
+    }
+
+    @Override
+    public void update(AnActionEvent e) {
+        super.update(e);
+        e.getPresentation().setIcon(AllIcons.Ide.HectorOff);
+    }
+}
